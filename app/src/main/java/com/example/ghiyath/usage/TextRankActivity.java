99a88e3e20c55561c31b015aab8e5b4ace814517
zsimjee <zaydsimjee@gmail.com>
2015-05-16 20:50:03 -0700

    added text results activity + layout. Not in Manifest.

---
+++ app/src/main/java/com/example/ghiyath/usage/TextRankActivity.java
@@ -0,0 +1,16 @@
+package com.example.ghiyath.usage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by z on 5/16/2015.
+ */
+public class TextRankActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_ranks);
+    }
+
+}
