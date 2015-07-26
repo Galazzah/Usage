185ce0e1bb244113fee2f4e4d2e3f8ae1827027c
Ghiyath Alazzah <ghiyath28@gmail.com>
2015-07-25 17:35:29 -0700

    working. Image adapter debugged

--- app/src/main/java/com/example/ghiyath/usage/MainActivity.java
+++ app/src/main/java/com/example/ghiyath/usage/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ghiyath.usage;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -32,57 +33,22 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button start = (Button) findViewById(R.id.start);
+        MessagesHandler m = new MessagesHandler(usageMap, topRanked, this);
+
+        usageMap = m.getUsage();
+        topRanked = m.getTopRanked(1);
+
+        ArrayAdapter<Contact> ranked = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1, topRanked);
+        ListView lv = (ListView) findViewById(R.id.lvMsg);
+        lv.setAdapter(ranked);
         start.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
-            Uri inboxUri = Uri.parse("content://sms/inbox");
 
-            String[] columns = new String[] { "_id", "address", "body" };
-
-            ContentResolver cr = getContentResolver();
-
-            Cursor c = cr.query(inboxUri, columns, null, null, null);
-
-            //populate HashMap usageMap with <double phone(phone nuber), Contact(phone)>
-            while(c.moveToNext()) {
-
-                String phone = c.getString(c.getColumnIndex("address"));
-                if(!usageMap.containsKey(phone))
-                    usageMap.put(phone, new Contact(String.valueOf(phone)));
-
-                Contact updatedContact = usageMap.get(phone);
-                updatedContact.incrementsmsReceived();
-                usageMap.put(phone, updatedContact);
-            }
-            //TEST
-            Contact testCon = new Contact("14083870968");
-            testCon.incrementsmsReceived();
-            testCon.incrementsmsReceived();
-            usageMap.put("14083870968", testCon);
-
-            Contact testCon2 = new Contact("14083870867");
-            testCon2.incrementsmsReceived();
-            testCon2.incrementsmsReceived();
-            testCon2.incrementsmsReceived();
-            usageMap.put("14083870867", testCon2);
-
-            //sort and populate ArrayList topRanked with Contact objects
-            for(String phoneNum : usageMap.keySet()) {
-                int i = 0;
-                for(; i < topRanked.size() && !usageMap.get(phoneNum).compareSMS(topRanked.get(i)); i++) {
-                }
-                topRanked.add(i, usageMap.get(phoneNum));
-            }
-
-            //ArrayAdapter to populate listView with entries from topRanked ArrayList
-            ArrayAdapter<Contact> ranked = new ArrayAdapter<Contact>(this,android.R.layout.simple_list_item_1, topRanked);
-            ListView lv = (ListView) findViewById(R.id.lvMsg);
-            lv.setAdapter(ranked);
 
 
 
--- app/src/main/java/com/example/ghiyath/usage/MessagesHandler.java
+++ app/src/main/java/com/example/ghiyath/usage/MessagesHandler.java
@@ -2,49 +2,71 @@ package com.example.ghiyath.usage;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Ghiyath on 5/16/2015.
  */
-public class MessagesHandler extends Activity{
-    private HashMap<String, Contact> usage;
+public class MessagesHandler {
+    private Map<String, Contact> usageMap;
     private ArrayList<Contact> topRanked;
 
+    public MessagesHandler( Map<String, Contact> usageMap, ArrayList<Contact> topRanked, Context context) {
+        this.usageMap = usageMap;
+        this.topRanked = topRanked;
 
-    public MessagesHandler() {
-        updateUsage();
-    }
-
-    public void updateUsage() {
-        //Read through SMS to:
-            //udpate usage
-            //update topRanked
-        /*
         Uri inboxUri = Uri.parse("content://sms/inbox");
-
         String[] columns = new String[] { "_id", "address", "body" };
-
-        ContentResolver cr = getContentResolver();
-
+        ContentResolver cr = context.getApplicationContext().getContentResolver();
         Cursor c = cr.query(inboxUri, columns, null, null, null);
-
-
-        c.moveToFirst();
+        //populate HashMap usageMap with <double phone(phone nuber), Contact(phone)>
         while(c.moveToNext()) {
-            Log.v("id", c.getString(c.getColumnIndex("_id")));
+            String phone = c.getString(c.getColumnIndex("address"));
+            Log.v("poop", phone);
+            if(!usageMap.containsKey(phone))
+                usageMap.put(phone, new Contact(String.valueOf(phone)));
 
-        } */
+            Contact updatedContact = usageMap.get(phone);
+            updatedContact.incrementsmsReceived();
+            usageMap.put(phone, updatedContact);
+        }
+        //TEST
+
+        Contact testCon = new Contact("14083870968");
+        testCon.incrementsmsReceived();
+        testCon.incrementsmsReceived();
+        usageMap.put("14083870968", testCon);
+
+        Contact testCon2 = new Contact("14083870867");
+        testCon2.incrementsmsReceived();
+        testCon2.incrementsmsReceived();
+        testCon2.incrementsmsReceived();
+        usageMap.put("14083870867", testCon2);
+
+        //sort and populate ArrayList topRanked with Contact objects
+        for(String phoneNum : usageMap.keySet()) {
+            int i = 0;
+            for(; i < topRanked.size() && !usageMap.get(phoneNum).compareSMS(topRanked.get(i)); i++) {
+            }
+            topRanked.add(i, usageMap.get(phoneNum));
+        }
+
 
     }
 
-    public HashMap<String, Contact> getUsage() {
-        return usage;
+    public Map<String, Contact> getUsage() {
+        return usageMap;
     }
 
     public ArrayList<Contact> getTopRanked(int ranks) {
